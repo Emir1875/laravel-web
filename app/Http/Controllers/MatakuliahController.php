8e23634dca49edcc00a7386d91f40a66fdc59cc3
mayakawai <maya24ti@mahasiswa.pcr.ac.id>
2025-09-29 13:46:04 +0700

    Initial commit

---
+++ app/Http/Controllers/MatakuliahController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MatakuliahController extends Controller
+{
+
+    public function index()
+    {
+        return "Menampilkan data matakuliah";
+    }
+    public function show($kode = null)
+    {
+        if ($kode == 'ST445') {
+            return "Anda mengakses matakuliah {$kode}";
+        } else {
+            return "Masukkan kode matakuliah!";
+        }
+    }
+}
---
+++ resources/views/halaman-mahasiswa-profil.blade.php
@@ -0,0 +1,6 @@
+<html lang="en">
+<body>
+    <h1>Selamat datang!</h1>
+    <h2>Ini adalah halaman Profil mahasiswa</h2>
+</body>
+</html>
