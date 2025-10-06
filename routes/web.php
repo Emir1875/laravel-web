84eb6f9fae36033cdda6a453b497b82d7bc5311f
mayakawai <maya24ti@mahasiswa.pcr.ac.id>
2025-10-06 13:56:15 +0700

    bener

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,38 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
+Route::get('/tes', function () {
     return view('welcome');
 });
 
 Route::get('/pcr', function () {
-    return ('Selamat Datang di Website kampus PCR');
+    return 'Selamat Datang di halaman PCR';
 });
 
 Route::get('/mahasiswa', function () {
-    return ('Hallo Mahasiwa');
+    return 'Hallo Mahasiswa';
 });
 
 Route::get('/nama/{param1}', function ($param1) {
-    return ('Nama saya: '.$param1);
+    return 'Nama saya : ' . $param1;
 });
 
-Route::get('/nim/{param1?}', function ($param1='') {
-    return ('Nim saya: '.$param1);
+Route::get('/nim/{param1}', function ($param1) {
+    return 'NIM saya : ' . $param1;
 });
 
-Route::get('/mahasiswa/{param1}',[MahasiswaController::class,'show']);
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']
+);
 
 Route::get('/about', function () {
     return view('halaman-about');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home',[HomeController::class, 'index']);
 
-Route::get('/matakuliah/show/{kode?}', [MatakuliahController::class, 'show']);
-
-Route::get('/home', [HomeController::class, 'index']);
+Route::post('question/store', [QuestionController::class, 'store'])
+        ->name('question.store');
